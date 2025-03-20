cfae66cfe084a80199ca97091908fe378579d4c1
Shubham Rai <shubhamrai01683@gmail.com>
2025-03-20 11:00:58 +0530

    first commit

---
+++ src/test/java/com/maven/hellospringboot/HellospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
